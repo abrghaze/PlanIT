cde446a37c82b91af75881f529f668f50762a364
abrghaze <amine.abaghraz23@ump.ac.ma>
2026-08-30 23:45:12 +0100

    fix: use package imports for privacy saver

--- mobile/lib/features/settings/data/privacy_file_saver.dart
+++ mobile/lib/features/settings/data/privacy_file_saver.dart
@@ -1,5 +1,5 @@
-import 'privacy_file_saver_stub.dart'
-    if (dart.library.io) 'privacy_file_saver_io.dart';
+import 'package:planit_mobile/features/settings/data/privacy_file_saver_stub.dart'
+    if (dart.library.io) 'package:planit_mobile/features/settings/data/privacy_file_saver_io.dart';
 
 Future<String> savePrivacyFile(String filename, List<int> bytes) =>
     savePrivacyFileImpl(filename, bytes);
